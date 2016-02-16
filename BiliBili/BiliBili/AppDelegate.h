4e77dee70746ae929a64a6095c48fb894c2c14b1
robinzhai <robinzhai@126.com>
2016-02-16 21:38:02 +0800

    第一次提交

---
+++ BiliBili/BiliBili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BiliBili
+//
+//  Created by zhaiguanjun on 16/2/16.
+//  Copyright © 2016年 翟冠军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BiliBili/BiliBili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BiliBili
+//
+//  Created by zhaiguanjun on 16/2/16.
+//  Copyright © 2016年 翟冠军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
